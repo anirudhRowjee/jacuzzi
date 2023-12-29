52052d0e56ecf297b7505e0534691cfbdfc9722a
Anirudh Rowjee <ani.rowjee@gmail.com>
2023-12-29 14:24:45 +0530

    feat: Added Cache Read with Constatn Frame Eviction Rule

--- jacuzzi.go
+++ jacuzzi.go
@@ -1,5 +1,10 @@
 package jacuzzi
 
+import (
+	"fmt"
+	"os"
+)
+
 /*
 # Jacuzzi - A concurrent Buffer Pool / Page Cache for Go
 
@@ -27,31 +32,105 @@ func (f *Frame) Initialize(p *PageCache, frameId int) {
 	f.PageId = 0
 }
 
-// map of the start offset to the page Frame itself Maybe storing it all in a map isn't the best idea? Feel like I should decouple the address mapping and the Frame access. Frames never get destructed and can be amortized on startup in terms of alloc cost, but these indices constantly change
+// Reset a frame to reuse it
+func (f *Frame) Reset() {
+	f.frameId = 0
+	f.Dirty = false
+	f.Pinned = false
+	f.PageId = 0
+}
+
 type PageCache struct {
-	PageSize  int
+	PageSize  int         // const: number of bytes in a page, loaded at runtime
 	Data      map[int]int // Data is the mapping of address to frames
 	Frames    []Frame     // Frames is where all page frames live, never get deleted
-	SlotCount int
+	SlotCount int         // the number of slots this pagecache has
+	file      *os.File    // backing file
 }
 
 // Start the pagecache
-func (p *PageCache) Init(PageSize, slots int) {
+func (p *PageCache) Init(PageSize, slots int, filename string) error {
 	p.PageSize = PageSize
 	p.SlotCount = slots
-	p.Data = make(map[int]int, p.SlotCount)
+
+	// Open the File
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 066)
+	if err != nil {
+		return err
+	}
+	p.file = file
+
+	// Allocate all Maps
+	p.Data = make(map[int]int, p.SlotCount) // TODO see if we need this to be constant-sized
 	p.Frames = make([]Frame, p.SlotCount)
-	// Initialize all allocs for frame
+
+	// Allocate all Frames
 	for idx := range p.Frames {
 		p.Frames[idx].Initialize(p, idx)
 	}
+	return nil
+}
+
+func CheckMapKeyPresent(mymap *map[int]int, target int) bool {
+	for k, _ := range *mymap {
+		if k == target {
+			return true
+		}
+	}
+	return false
 }
 
 // A function that reads a page into a buffer
 // return signature (bool, error) ->
 // ----> bool: True if Cache Hit, False if Cache Miss
 func (p *PageCache) ReadPage(PageStartAddress int, Destination []byte) (bool, error) {
-	return false, nil
+
+	// Check if the offset exists in the translation table
+	hit := CheckMapKeyPresent(&p.Data, PageStartAddress)
+
+	if hit {
+		// If it exists, copy the page into the destination
+		TargetFrameId := p.Data[PageStartAddress]
+		BytesCopied := copy(Destination, p.Frames[TargetFrameId].data)
+		if BytesCopied != p.PageSize {
+			return hit, fmt.Errorf(
+				"Could not Copy a Full Page -> Expected %d Copied %d",
+				p.PageSize,
+				BytesCopied,
+			)
+		}
+
+	} else {
+		// If it doesn't exist, evict a page, and fill the frame with the part necessary
+		// As of right now, this just returns the first page
+		NewFrameId := p.EvictPage()
+		// Reset the new Frame
+		p.Frames[NewFrameId].Reset()
+		// Copy the data from the file into the new frame
+		bytes_read, err := p.file.ReadAt(p.Frames[NewFrameId].data, int64(PageStartAddress))
+		if err != nil {
+			return hit, err
+		}
+		if bytes_read != p.PageSize {
+			return hit, fmt.Errorf(
+				"Could not Copy a Full Page -> Expected %d Copied %d",
+				p.PageSize,
+				bytes_read,
+			)
+		}
+		// Add the mapping of the offset into the data table
+		p.Data[PageStartAddress] = NewFrameId
+		// Finally, read into target buffer
+		BytesCopied := copy(Destination, p.Frames[NewFrameId].data)
+		if BytesCopied != p.PageSize {
+			return hit, fmt.Errorf(
+				"Could not Copy a Full Page -> Expected %d Copied %d",
+				p.PageSize,
+				BytesCopied,
+			)
+		}
+	}
+	return hit, nil
 }
 
 // A function that writes a page from the application to the pagecache
@@ -60,3 +139,21 @@ func (p *PageCache) ReadPage(PageStartAddress int, Destination []byte) (bool, er
 func (p *PageCache) WritePage(PageStartAddress int, Content *[]byte) (bool, error) {
 	return false, nil
 }
+
+// A function that writes a page frame from the cache to the disk
+func (p *PageCache) FlushFrameToDisk(FrameID int) (bool, error) {
+
+	return false, nil
+}
+
+// function to evict page and return pageId of new empty frame
+func (p *PageCache) EvictPage() int {
+
+	return 0
+}
+
+// TODO
+// Add functions for
+// -> Flushing a Page to Disk
+// -> Evicting a Page from the Cache
+// ->
--- jacuzzi_test.go
+++ jacuzzi_test.go
@@ -1,24 +1,23 @@
 package jacuzzi
 
 import (
+	"bytes"
 	"os"
 	"testing"
 )
-
-func TestEverythingWorks(t *testing.T) {
-	if (1 + 1) != 2 {
-		t.Error("Addition is not working!")
-	}
-}
 
 // test to see if the pagecache will initialize
 func TestInitializePageCache(t *testing.T) {
 
 	pageSize := os.Getpagesize()
 	slots := 10
+	file := "test.bin"
 
 	var pc PageCache = PageCache{}
-	pc.Init(pageSize, slots)
+	err := pc.Init(pageSize, slots, file)
+	if err != nil {
+		t.Error(err)
+	}
 
 	// Check if all the frames have been initialized
 	if pc.SlotCount != slots {
@@ -36,5 +35,42 @@ func TestInitializePageCache(t *testing.T) {
 			len(pc.Frames),
 			pc.SlotCount,
 		)
+	}
+}
+
+// Check if reading into the pagecache works
+func TestReadIntoCache(t *testing.T) {
+
+	pageSize := os.Getpagesize()
+	slots := 10
+	file := "test.bin"
+
+	var pc PageCache = PageCache{}
+	err := pc.Init(pageSize, slots, file)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Read the first page of the file into memory
+	buffer1 := make([]byte, pageSize)
+	hit1, err := pc.ReadPage(0, buffer1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	buffer2 := make([]byte, pageSize)
+	hit2, err := pc.ReadPage(0, buffer2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if hit1 != false {
+		t.Error("Unexpected Cache Hit on first read of page, hit status: ", hit1)
+	}
+	if hit2 != true {
+		t.Error("Unexpected Cache Miss on second read of page, hit status: ", hit2)
+	}
+	if bytes.Equal(buffer1, buffer2) != true {
+		t.Errorf("Data Drift in bytes read from cache: Inconsistency between first and second read")
 	}
 }
